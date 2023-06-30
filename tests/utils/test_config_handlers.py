120420432dddeb94b666bc61c920f8a5505cc579
nikdemm <nikdemm@gmail.com>
2023-07-01 00:37:20 +0100

    Refactor test_config_handlers.py

--- tests/utils/test_config_handlers.py
+++ tests/utils/test_config_handlers.py
@@ -4,33 +4,36 @@ import os
 
 from jiant.utils import config_handlers
 
+BASE_PATH = os.path.dirname(__file__)
+
+def load_json(path):
+    with open(path, 'r') as file:
+        return json.load(file)
 
 def test_json_merge_patch():
     """Tests that JSON Merge Patch works as expected (https://tools.ietf.org/html/rfc7396)"""
-    target = """
-    {
+    target = {
         "title": "Goodbye!",
         "author" : {
             "givenName" : "John",
             "familyName" : "Doe"
         },
-        "tags":[ "example", "sample" ],
+        "tags": [ "example", "sample" ],
         "content": "This will be unchanged"
     }
-    """
-    patch = """
-    {
+    
+    patch = {
         "title": "Hello!",
         "phoneNumber": "+01-123-456-7890",
         "author": {
-            "familyName": null
+            "familyName": None
         },
         "tags": [ "example" ]
     }
-    """
-    merged = config_handlers.json_merge_patch(target, patch)
-    expected = """
-    {
+    
+    merged = config_handlers.json_merge_patch(json.dumps(target), json.dumps(patch))
+    
+    expected = {
         "title": "Hello!",
         "author" : {
             "givenName" : "John"
@@ -39,24 +42,19 @@ def test_json_merge_patch():
         "content": "This will be unchanged",
         "phoneNumber": "+01-123-456-7890"
     }
-    """
-    merged_sorted: str = json.dumps(json.loads(merged), sort_keys=True)
-    expected_sorted: str = json.dumps(json.loads(expected), sort_keys=True)
-    assert merged_sorted == expected_sorted
+    
+    assert json.dumps(json.loads(merged), sort_keys=True) == json.dumps(expected, sort_keys=True)
 
 
 def test_merging_multiple_json_configs():
-    with open(os.path.join(os.path.dirname(__file__), "config/base_config.json")) as f:
-        base_config = f.read()
-    with open(os.path.join(os.path.dirname(__file__), "./config/first_override_config.json")) as f:
-        override_config_1 = f.read()
-    with open(os.path.join(os.path.dirname(__file__), "./config/second_override_config.json")) as f:
-        override_config_2 = f.read()
+    base_config = load_json(os.path.join(BASE_PATH, "config/base_config.json"))
+    override_config_1 = load_json(os.path.join(BASE_PATH, "./config/first_override_config.json"))
+    override_config_2 = load_json(os.path.join(BASE_PATH, "./config/second_override_config.json"))
+    
     merged_config = config_handlers.merge_jsons_in_order(
-        [base_config, override_config_1, override_config_2]
+        [json.dumps(base_config), json.dumps(override_config_1), json.dumps(override_config_2)]
     )
-    with open(os.path.join(os.path.dirname(__file__), "./config/final_config.json")) as f:
-        expected_config = f.read()
-    sorted_merged_config = json.dumps(json.loads(merged_config), sort_keys=True)
-    sorted_expected_config = json.dumps(json.loads(expected_config), sort_keys=True)
-    assert sorted_merged_config == sorted_expected_config
+    
+    expected_config = load_json(os.path.join(BASE_PATH, "./config/final_config.json"))
+    
+    assert json.dumps(json.loads(merged_config), sort_keys=True) == json.dumps(expected_config, sort_keys=True)
